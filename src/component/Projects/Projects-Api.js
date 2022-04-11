45c3d44f6577ce0816675e0362fb64b19eacbbe1
tigerbhai16 <suryanshsoni16@gmail.com>
2022-04-11 14:13:12 +0530

    contact image responsiveness issue fixed

--- src/component/Projects/Projects-Api.js
+++ src/component/Projects/Projects-Api.js
@@ -8,7 +8,7 @@ const data = [
   {
     id: 2,
     title: "HTML,CSS,JS",
-    desc: " Projects based on using tecnologies such as HTML,CSS,JS,Bootstrap",
+    desc: " Projects based on  HTML,CSS,JS,Bootstrap",
     link : "https://github.com/tigerbhai16/HTML-CSS-JS",
    
   },
@@ -16,20 +16,20 @@ const data = [
   {
     id: 3,
     title: "CryptoWorld",
-    desc: "CryptoWorld is full fledge react app , where user can watch all infromation about cryptocurriencies , latest trends , user can also create account and add and save crypto to their watchlist for future. ",
+    desc: "CryptoWorld is full fledge react app made using ReactJs ,Matrial-UI,Firebase, where user can watch all infromation about cryptocurriencies , latest trends , user can create account add and save crypto to their watchlist for future. ",
     link : "https://tranquil-mandazi-b7672d.netlify.app/",
   },
 
   {
     id: 4,
     title: "Classic Chat App",
-    desc: "This is a realtime group chat application where multiply user can chat,send pictures, also admin user can create, add ,remove, users at realtime ",
+    desc: "This is a realtime group chat application made using ReactJS , chatengine  where multiply user can chat , send pictures , also admin user can create, add ,remove, users at realtime ",
     link :"https://harmonious-crisp-fa6a4b.netlify.app/",
   },
   {
     id: 5,
     title: "WordHunt",
-    desc: " It is a Dictionary app made using ReactJS material-ui In which user can search a word in over 12 languages will get the result ,example,audio and synonyms of word . can also use darl and loght mode",
+    desc: " It is a Dictionary app made using ReactJS material-ui In which user can search a word in over 12 languages will get the result ,example,audio and synonyms of word . can also use dark and light mode",
     link :"https://vermillion-cendol-24f23d.netlify.app",
   },
 ]
